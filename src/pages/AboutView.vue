18db6e840db49fd9224fd72c0cc8666a040dfd7b
李紫鑫 <lizixin@boco.com.cn>
2022-03-01 21:25:48 +0800

    fix: test

--- src/pages/AboutView.vue
+++ src/pages/AboutView.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-03-01 21:21:34
+ * @LastEditTime: 2022-03-01 21:25:34
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /low-code/low-code-vue/src/pages/AboutView.vue
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
   </div>
 </template>
