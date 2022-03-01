1cf62aa273009e0450d7759e856a1c14eb69fec6
李紫鑫 <lizixin@boco.com.cn>
2022-03-01 22:52:16 +0800

    fix: test

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2022-03-01 21:21:34
- * @LastEditTime: 2022-03-01 22:20:36
- * @LastEditors: your name
+ * @LastEditTime: 2022-03-01 22:52:00
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /low-code/low-code-vue/vue.config.js
  */
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   publicPath: "./",
-});
+};
