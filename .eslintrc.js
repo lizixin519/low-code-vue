33df55d1478d09cb9367dcae0b099810e87954d8
李紫鑫 <lizixin@1087826336064877.onaliyun.com>
2022-03-01 13:35:23 +0800

    feat: 项目初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-01 13:22:19
+ * @LastEditTime: 2022-03-01 13:23:38
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /low-code-vue/.eslintrc.js
+ */
 module.exports = {
   root: true,
   env: {
@@ -13,7 +21,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+    'prefer-const': 0
   },
   overrides: [
     {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-03-01 13:22:19
+ * @LastEditTime: 2022-03-01 13:28:59
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /low-code-vue/src/views/HomeView.vue
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
