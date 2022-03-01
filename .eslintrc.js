accda42ebe6925713425e741fd866b3a7e6764d5
李紫鑫 <lizixin@1087826336064877.onaliyun.com>
2022-03-01 18:03:12 +0800

    feat: 增加测试页面

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-03-01 13:22:19
- * @LastEditTime: 2022-03-01 13:23:38
- * @LastEditors: your name
+ * @LastEditTime: 2022-03-01 15:31:56
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /low-code-vue/.eslintrc.js
  */
@@ -11,11 +11,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
@@ -23,14 +19,13 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0,
-    'prefer-const': 0
+    'prefer-const': 0,
+    'eol-last': 0,
+    'vue/multi-word-component-names': 0
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
